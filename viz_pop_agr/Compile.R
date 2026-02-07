4b4d4e13e2062137db7591f589171a5379606d6a
Tbtaaa <julissa.condor.25@ucl.ac.uk>
2026-02-07 15:41:43 +0000

    countries fixed

--- viz_pop_agr/Compile.R
+++ viz_pop_agr/Compile.R
@@ -1,6 +1,9 @@
+install.packages("lwgeom")
+
 library(sf)
 library(tidyverse)
 library(countrycode)
+library(lwgeom)
 
 agr_africa <- read_csv("data/FAOSTAT_data_en_1-30-2026_Africa.csv") %>%
   select('Area', 'Year', 'Value') %>%
@@ -70,69 +73,113 @@ pop_world <- read_csv("data/API_SP.POP.TOTL_DS2_en_csv_v2_7.csv", skip = 4) %>%
 
 # geometries ###################################################################
 
-countries_world <- st_read("data/World Bank Official Boundaries - Admin 0_all_layers/WB_GAD_ADM0_complete.shp") %>%
-  filter(WB_STATUS == "Member State",!is.na(ISO_A3)) %>%
+# read and filter columns
+countries_world_raw <- st_read("data/World Bank Official Boundaries - Admin 0_all_layers/WB_GAD_ADM0_complete.shp") %>%
   select("ISO_A3","NAM_0","geometry") %>%
   rename(Country_Code = ISO_A3,
-         Country_Name = NAM_0)
+         Country_Name = NAM_0) %>%
+  st_make_valid() %>%
+  st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180"))
 
-#-------------------------------------------------------------------------------
+# count countries per code
+count_per_code <- countries_world_raw %>%
+  st_drop_geometry() %>%
+  count(Country_Code)
 
-countries_world <- countries_world %>%
+# keep bigger areas or combine
+codes_union   <- c("UMI", "BES", "UKR", "ZWE")        
+codes_largest <- c("ESP", "PRT", "AUS", "FRA", "GBR") 
+
+# combine
+sf_union <- countries_world_raw %>%
+  filter(Country_Code %in% codes_union) %>%
+  group_by(Country_Code) %>%                    
+  summarise(Country_Name = first(Country_Name),   
+            geometry = st_union(geometry),
+            .groups = "drop") %>%
+  mutate(.area = as.numeric(st_area(geometry)))
+
+# keep bigger
+sf_largest <- countries_world_raw %>%
+  filter(Country_Code %in% codes_largest) %>%
+  mutate(.area = as.numeric(st_area(geometry))) %>%
+  group_by(Country_Code) %>%
+  slice_max(order_by = .area, n = 1, with_ties = FALSE) %>% 
+  ungroup() 
+
+# the rest
+sf_rest <- countries_world_raw %>%
+  filter(!Country_Code %in% c(codes_union, codes_largest)) %>%
+  mutate(.area = as.numeric(st_area(geometry)))
+
+# merge
+countries_world_filt <- bind_rows(sf_rest, sf_union, sf_largest)
+  
+countries_world_rename <- countries_world_filt %>%
   mutate(Country_Name = case_when(
     Country_Code == "CIV" ~ "Côte d’Ivoire",
+    Country_Code == "CUW" ~ "Curazao",
     Country_Code == "STP" ~ "São Tomé and Príncipe",
-    Country_Code == "REU" ~ "eéunion",
+    Country_Code == "REU" ~ "Eéunion",
     Country_Code == "MYT" ~ "Mayotte",
     Country_Code == "GLP" ~ "Guadeloupe",
     Country_Code == "MTQ" ~ "Martinique",
     Country_Code == "GUF" ~ "French Guiana",
+    Country_Code == "BLM" ~ "San Bartolomé",
+    Country_Code == "TUR" ~ "Turkey",
+    Country_Code == "REU" ~ "Réunion",
     TRUE ~ Country_Name))
 
 # combine ######################################################################
 
-world_pop <- countries_world %>%
+world_pop <- countries_world_rename %>%
   left_join(pop_world,
             by = c("Country_Code" = "Country Code")) %>% 
-  drop_na() %>%
   pivot_longer(cols = matches("^[0-9]{4}$"),
                names_to = "year",
                values_to = "Pop_value") %>%
   mutate(year = as.integer(year)) %>%
   st_drop_geometry() %>%
-  select(Country_Code, Country_Name, everything(), -Country)
+  select(Country_Code, Country_Name, everything(), -Country, -.area)
+
+colnames(world_pop)
 
 #-------------------------------------------------------------------------------
 
 year_cols <- names(agr_world)[grepl("^[0-9]{4}$", names(agr_world))]
 
-world_agr_1 <- countries_world %>%
+# join sets
+world_agr_1 <- countries_world_rename %>%
   left_join(agr_world,
             by = c("Country_Code" = "iso3")) %>%
   st_transform(6933) %>%
-  st_make_valid() %>%
-  group_by(Country_Code, Country_Name) %>%
-  summarise(
-    across(all_of(year_cols), first),
-    geometry = st_union(geometry),
-    .groups = "drop")
+  group_by(Country_Code, Country_Name, .area)
 
+colnames(world_agr_1)
+
+# calculate percentage
 world_agr_2 <- world_agr_1 %>% 
-  mutate(country_area_m2 = as.numeric(st_area(geometry))) %>%
   mutate(across(all_of(year_cols),
-                ~ (.x * 1e7 / country_area_m2) * 100)) %>%
-  distinct(Country_Code, .keep_all = TRUE)
+                ~ (.x * 1e7 / .area) * 100))
 
+colnames(world_agr_2)
+
+# pivot longer
 world_agr_3 <- world_agr_2 %>%
   st_drop_geometry()
+
+colnames(world_agr_3)
 
 world_agr <- world_agr_3 %>%
   pivot_longer(
     cols = matches("^[0-9]{4}$"),
     names_to = "year",
     values_to = "Crop_per") %>%
-  mutate(year = as.integer(year))%>%
-  select(Country_Code, Country_Name, everything(), -country_area_m2)
+  mutate(year = as.integer(year)) %>%
+  ungroup() %>% 
+  select(-.area, -Country)
+
+colnames(world_agr)
 
 # save #########################################################################
 
@@ -142,7 +189,7 @@ st_write(world_pop,
 st_write(world_agr,
          "data/world_agr.csv")
 
-st_write(countries_world,
+st_write(countries_world_rename,
          "data/World_boundaries/world_boundaries.shp")
 
 #lokk for NULL values
